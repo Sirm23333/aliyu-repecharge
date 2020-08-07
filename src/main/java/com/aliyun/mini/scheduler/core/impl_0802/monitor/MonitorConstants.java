b7ac757b6b18f91516647963e45b741d5c2f2f1f
Sirm23333 <sunyuping2018@163.com>
2020-08-08 01:44:14 +0800

    8.8 简单调参

--- src/main/java/com/aliyun/mini/scheduler/core/impl_0802/monitor/MonitorConstants.java
+++ src/main/java/com/aliyun/mini/scheduler/core/impl_0802/monitor/MonitorConstants.java
@@ -5,7 +5,7 @@ package com.aliyun.mini.scheduler.core.impl_0802.monitor;
  */
 public class MonitorConstants {
     // 同步stats周期，单位ms
-    public static final long SYN_NODE_STATS_CYC = 50;
+    public static final long SYN_NODE_STATS_CYC = 80;
     // 保存近几个周期的nodeStats信息
     public static final int SAVE_NODE_STATS_CYC_CNT = 600;
 }
--- src/main/java/com/aliyun/mini/scheduler/core/impl_0802/node_container_manager/NodeContainerManagerContants.java
+++ src/main/java/com/aliyun/mini/scheduler/core/impl_0802/node_container_manager/NodeContainerManagerContants.java
@@ -10,7 +10,7 @@ public class NodeContainerManagerContants {
     // 允许同时申请node的最大线程数
     public static int RESERVE_NODE_CONCURRENT_UPPER = 1;
     // 最多允许使用的node数
-    public static int MAX_NODE_NUM = 18;
+    public static int MAX_NODE_NUM = 15;
     // 清理container时使用多级缓存置换+LRU算法，对响应时间小于以下阈值时间(ms)的container做为低优先清除的container
     public static long CONTAINER_CLEAN_AVG_DURATION_LOWER = 100;
     // 低优先级清除的container需要标记CONTAINER_CLEAN_SIGN_CNT次才可以被删除
